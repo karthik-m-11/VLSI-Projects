7f57db02092e2afbca08f1d2be7aca88e0bec1c6
karthik-m-11 <karthik.mukund2002@gmail.com>
2025-12-20 16:37:47 +0530

    Delete Design-Verification/AMBA-AHB-Design-and-Verification-using-UVM/source_code directory

--- Design-Verification/AMBA-AHB-Design-and-Verification-using-UVM/source_code/sequencer.sv
+++
@@ -1,8 +0,0 @@
-// uvm_env/sequencer.sv
-class sequencer extends uvm_sequencer #(seq_item);
-  `uvm_component_utils(sequencer)
-
-  function new(string name = "sequencer", uvm_component parent = null);
-    super.new(name, parent);
-  endfunction
-endclass
